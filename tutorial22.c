5839257b37fcb6110d1bc267481ebbad3a6f560b
Nikhil <nikhil788p@gmail.com>
2020-07-16 11:41:43 +0530

    writing a program

--- tutorial22.c
+++ tutorial22.c
@@ -1,15 +1,17 @@
 /*
-
 write a program to  convert each as given
 use recursion and function
-
 kms to miles
 inches to foot
 cms to incles 
 pounds to kg
 inches to meters
-
-
-
-
 */
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello World\n");
+    return 0;
+}
